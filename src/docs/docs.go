8fe94cfd1de6fd67271092c752a62e9d98df64d6
Sooyoung Kim <sykim@etri.re.kr>
2020-12-03 06:45:16 +0000

    docs: Fix version in swagger

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -537,7 +537,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "1.0",
+	Version:     "0.3.0-espresso",
 	Host:        "localhost:8080",
 	BasePath:    "/ladybug",
 	Schemes:     []string{},
